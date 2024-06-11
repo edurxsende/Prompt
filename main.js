ca17b9de67effcf030e9038fe12ea2bb7525a9bf
Eduardo Melo <edurxsende@gmail.com>
2024-06-11 15:33:24 -0300

    Implementação de variavel const

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let fistName = prompt('Digite seu nome: ')
 let lastName = prompt('Digite seu sobrenome: ')
 let age = parseInt (prompt('Digite sua idade: '))
-
+const age = 4
 age = age + 2
 
 console.log (fistName)
