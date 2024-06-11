f479f801d722782dc403d4349188e70e62f8f855
Eduardo Melo <edurxsende@gmail.com>
2024-06-11 00:49:09 -0300

    Usando Prompt para solicitar variaveis

---
+++ main.js
@@ -0,0 +1,9 @@
+let fistName = prompt('Digite seu nome: ')
+let lastName = prompt('Digite seu sobrenome: ')
+let age = parseInt (prompt('Digite sua idade: '))
+
+age = age + 2
+
+console.log (fistName)
+console.log (lastName)
+console.log (age)
